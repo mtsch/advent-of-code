63233cd188a1614323571ed2fce0729cecc19b79
mtsch <matijacufar@gmail.com>
2015-12-07 11:06:16 +0100

    shorter version of day 2

--- day2.R
+++ day2.R
@@ -1,9 +1,8 @@
 gifts <- as.matrix(read.table("input2.txt", sep="x"))
 
-sides <- function(v)
-  v * v[c(3, 1, 2)]
-
-paperNeeded <- function(sides)
+paperNeeded <- function(v) {
+  sides <- v * v[c(3, 1, 2)]
   2 * sum(sides) + min(sides)
+}
 
-result <- sum(apply(gifts, 1, function(g) paperNeeded(sides(g))))
+result <- sum(apply(gifts, 1, paperNeeded))
